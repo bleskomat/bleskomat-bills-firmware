12c58ff0a280a1bc9a297b0c1ad67c926e36fe2e
Charles Hill <chill@degreesofzero.com>
2020-05-23 13:14:32 +0200

    Remove un-used code from utilstrencodings

--- src/lnurl.cpp
+++ src/lnurl.cpp
@@ -12,10 +12,10 @@ namespace {
 	}
 
 	std::string bech32_encode(const std::string& hrp, const std::string& value) {
-		std::vector<uint8_t> v = ParseHex(string_to_hex(value));
+		std::vector<uint8_t> v = utilstrencodings::ParseHex(string_to_hex(value));
 		std::vector<unsigned char> tmp = {};
 		tmp.reserve(1 + 32 * 8 / 5);
-		ConvertBits<8, 5, true>([&](unsigned char c) { tmp.push_back(c); }, v.begin(), v.end());
+		utilstrencodings::ConvertBits<8, 5, true>([&](unsigned char c) { tmp.push_back(c); }, v.begin(), v.end());
 		std::string s(v.begin(), v.end());
 		return bech32::encode(hrp, tmp);
 	}
