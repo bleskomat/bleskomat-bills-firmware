871e9e106fca39346096ecca61964c312509bb0c
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2020-06-09 23:34:06 +0200

    feat: bill acceptor integratio module

--- include/modules/bill-acceptor.h
+++ include/modules/bill-acceptor.h
@@ -1,5 +1,23 @@
 #include "logger.h"
+#include <SoftwareSerial.h>
+#include <map>
+
+#ifndef BILL_ACCEPTOR_TX_PIN
+	#define BILL_ACCEPTOR_TX_PIN 1
+#endif
+
+#ifndef BILL_ACCEPTOR_RX_PIN
+	#define BILL_ACCEPTOR_RX_PIN 3
+#endif
+
+#ifndef BILL_ACCEPTOR_DATA_RATE
+	#define BILL_ACCEPTOR_DATA_RATE 300
+#endif
 
 namespace billAcceptor {
 	void init();
+	void loop();
+	float getAccumulatedValue();
+	void setFiatCurrency(const std::string &fiatCurrency);
+	void reset();
 }
--- src/modules/bill-acceptor.cpp
+++ src/modules/bill-acceptor.cpp
@@ -1,7 +1,57 @@
 #include "modules/bill-acceptor.h"
 
+namespace {
+	std::string fiatCurrency = "";
+	float VALUE_ACCUMULATED = 0.00;
+
+	// Mapping serial codes comming form NV10:
+	// EUR {code, value} mappings
+	std::map<int,int> eur{ {1, 5.0}, {2, 10.0}, {3, 20.0}, {4, 50.0}, {5, 100.0}, {6, 200.0} };
+	// CZK {code, value} mappings
+	std::map<int,int> czk{ {7, 100.0}, {8, 200.0}, {9, 500.0}, {10, 1000.0}, {11, 2000.0}, {12, 5000.0} };
+
+	float getValueIncrement(byte byteIn) {
+		logger::write(fiatCurrency);
+		if (fiatCurrency == "EUR") {
+			return eur[byteIn];
+		}
+		return czk[byteIn];
+	}
+
+	void incrementAccumulatedValue(byte byteIn) {
+		VALUE_ACCUMULATED = VALUE_ACCUMULATED + getValueIncrement(byteIn);
+	}
+}
+
 namespace billAcceptor {
 
+	SoftwareSerial BillAcceptor(BILL_ACCEPTOR_RX_PIN, BILL_ACCEPTOR_TX_PIN); // RX, TX
+
 	void init() {
+		// set the data rate for the SoftwareSerial port
+		BillAcceptor.begin(BILL_ACCEPTOR_DATA_RATE);
+	}
+
+	void loop() {
+
+		if (BillAcceptor.available()) {
+			byte byteIn = BillAcceptor.read();
+
+			if (byteIn >= 1 && byteIn <= 12) {
+				incrementAccumulatedValue(byteIn);
+			}
+		}
+	}
+
+	float getAccumulatedValue() {
+		return VALUE_ACCUMULATED;
+	}
+
+	void setFiatCurrency(const std::string &str) {
+		fiatCurrency = str;
+	}
+
+	void reset() {
+		VALUE_ACCUMULATED = 0.00;
 	}
 }
