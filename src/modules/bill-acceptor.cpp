afc7ceb281466324410120e03d9911228642974a
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2021-01-21 10:03:25 +0100

    feat: reject bills after buyLimit

--- src/modules/bill-acceptor.cpp
+++ src/modules/bill-acceptor.cpp
@@ -3,7 +3,9 @@
 namespace {
 
 	std::vector<float> billValues;
+	std::deque<int> buffer;
 	float accumulatedValue = 0.00;
+	float escrowValue = 0.00;
 
 	float getBillValue(const int &byteIn) {
 		const int index = byteIn - 1;
@@ -11,6 +13,27 @@ namespace {
 			return billValues[index];
 		}
 		return 0;
+	}
+
+	void parseBuffer() {
+		while (buffer.size() >= 3) {
+			const int byte1 = buffer.front();
+			buffer.pop_front();
+			if (byte1 == BILL_ACCEPTOR_CODE_IS_BUSY) {
+				const int byte2 = buffer.front();
+				buffer.pop_front();
+				if (byte2 == BILL_ACCEPTOR_CODE_NOT_BUSY) {
+					// The next byte should be the bill value reference.
+					const int byte3 = buffer.front();
+					buffer.pop_front();
+					float billValue = getBillValue(byte3);
+					if (billValue > 0) {
+						logger::write("Bill inserted with value = " + util::floatToString(billValue));
+						escrowValue += billValue;
+					}
+				}
+			}
+		}
 	}
 }
 
@@ -20,24 +43,26 @@ namespace billAcceptor {
 		billValues = config::getBillValues();
 		Serial1.begin(BILL_ACCEPTOR_DATA_RATE, SERIAL_8N1, BILL_ACCEPTOR_RX_PIN, BILL_ACCEPTOR_TX_PIN);
 		billAcceptor::on();
+		billAcceptor::enableEscrowMode();
 	}
 
 	void loop() {
-		if (Serial1.available()) {
+		while (Serial1.available()) {
 			byte byteIn = Serial1.read();
 			if (byteIn > 0) {
 				logger::write("Bill acceptor byte received: " + util::byteToString(byteIn));
-				float billValue = getBillValue(byteIn);
-				if (billValue > 0) {
-					logger::write("Bill inserted with value = " + util::floatToString(billValue));
-					accumulatedValue += billValue;
-				}
+				buffer.push_back(byteIn);
 			}
 		}
+		parseBuffer();
 	}
 
 	float getAccumulatedValue() {
 		return accumulatedValue;
+	}
+
+	float getEscrowValue() {
+		return escrowValue;
 	}
 
 	void reset() {
@@ -52,5 +77,28 @@ namespace billAcceptor {
 	void off() {
 		logger::write("Switching bill acceptor OFF");
 		Serial1.write(BILL_ACCEPTOR_CODE_DISABLE_ALL);
+	}
+
+	void enableEscrowMode() {
+		logger::write("Enabling escrow mode in bill acceptor");
+		Serial1.write(BILL_ACCEPTOR_CODE_ENABLE_ESCROW);
+	}
+
+	void acceptEscrow() {
+		logger::write("Accepting escrow in bill acceptor");
+		accumulatedValue += escrowValue;
+		Serial1.write(BILL_ACCEPTOR_CODE_ACCEPT_ESCROW);
+		billAcceptor::clearEscrowValue();
+	}
+
+	void rejectEscrow() {
+		logger::write("Rejecting escrow in bill acceptor");
+		Serial1.write(BILL_ACCEPTOR_CODE_REJECT_ESCROW);
+		billAcceptor::clearEscrowValue();
+	}
+
+	void clearEscrowValue() {
+		logger::write("Clearing escrow value in bill acceptor");
+		escrowValue = 0.0;
 	}
 }
