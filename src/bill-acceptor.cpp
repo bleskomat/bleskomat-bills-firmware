ad86b9748125ea73f86900202c24a6054d286966
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-10-19 16:47:16 +0200

    fix(bill-acceptor): wait for escrow mode confirmation

--- src/bill-acceptor.cpp
+++ src/bill-acceptor.cpp
@@ -16,6 +16,7 @@ namespace {
 	unsigned short billTxPin;
 	unsigned short billRxPin;
 	unsigned int billBaudRate;
+	bool ackEscrowMode = false;
 
 	const std::map<const char*, const uint8_t> SIO_Codes = {
 		{ "note_accepted_c1", 1 },
@@ -164,12 +165,18 @@ namespace billAcceptor {
 	void loop() {
 		if (state == State::initialized) {
 			while (Serial1.available()) {
+				if (!ackEscrowMode) {
+					billAcceptor::enableEscrow();
+				}
 				const uint8_t byteIn = Serial1.read();
 				if (byteIn > 0) {
 					const std::string code = getSIOCodeKey(byteIn);
 					if (code != "") {
 						logger::write("Bill acceptor code received: " + code);
 						buffer.push_back(byteIn);
+						if (code == "enable_escrow_mode") {
+							ackEscrowMode = true;
+						}
 					}
 				}
 			}
@@ -188,8 +195,6 @@ namespace billAcceptor {
 				logger::write("Initializing bill acceptor...");
 				Serial1.begin(billBaudRate, SERIAL_8N1, billTxPin, billRxPin);
 				billAcceptor::disinhibit();
-				serialWriteSIOCode("enable_escrow_mode");
-				serialWriteSIOCode("enable_escrow_timeout");
 				state = State::initialized;
 			}
 		}
@@ -213,6 +218,11 @@ namespace billAcceptor {
 
 	void inhibit() {
 		serialWriteSIOCode("disable_all");
+	}
+
+	void enableEscrow() {
+		serialWriteSIOCode("enable_escrow_mode");
+		serialWriteSIOCode("enable_escrow_timeout");
 	}
 
 	void acceptEscrow() {
